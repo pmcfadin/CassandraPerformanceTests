1254fcab00982fca2399a6d15bbc841708fa52e4
Patrick McFadin <pmcfadin@gmail.com>
2016-04-07 14:14:41 -0700

    Changed calculation on time series sequence

--- src/com/datastax/example/TimeSeriesInsert.java
+++ src/com/datastax/example/TimeSeriesInsert.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.UUID;
 
 /**
@@ -27,7 +28,7 @@ public class TimeSeriesInsert extends TestBase {
         logger.info("Beginning TimeSeriesInsert:load");
 
         Calendar baseTime = Calendar.getInstance();
-        baseTime.add(Calendar.HOUR, 24);
+        baseTime.add(Calendar.HOUR, 1);
 
         long timeToStop = baseTime.getTimeInMillis();
 
@@ -36,20 +37,32 @@ public class TimeSeriesInsert extends TestBase {
         BoundStatement recordInsert = new BoundStatement(recordInsertStatement);
 
         long now = System.currentTimeMillis();
+
         long recordCount = 0;
         UUID id;
+
+        // Set time series epoch date to Jan 1, 2000 at midnight
+        Calendar timeseriesEpoch = new GregorianCalendar(2000,1,1,0,0,0);
+
 
         while(now < timeToStop){
             now = System.currentTimeMillis();
             id = UUID.randomUUID();
 
-            // Store one minute of data per partition
-            for(int i = 0; i < 60000; i++) {
+            // Store one day of second resolution data per partition
+            for(int i = 0; i < 86400; i++) {
+
                 // Use execute to make sure each record is inserted and the return code is correct.
-                session.execute(recordInsert.bind(id, new java.sql.Date(System.currentTimeMillis()), RandomStringUtils.randomAlphabetic(1024)));
-            }
-            if(recordCount%10000 == 0){
-                logger.info("Records loaded: " + recordCount);
+                session.execute(recordInsert.bind(id, new java.sql.Date(timeseriesEpoch.getTimeInMillis()), RandomStringUtils.randomAlphabetic(1024)));
+                recordCount++;
+
+                //Advance the epoch by one second
+                timeseriesEpoch.add(Calendar.SECOND, 1);
+
+
+                if((recordCount % 100000) == 0){
+                    logger.info("Records loaded: " + recordCount);
+                }
             }
         }
         logger.info("Ending TimeSeriesInsert:load. Total records loaded: " + recordCount);
